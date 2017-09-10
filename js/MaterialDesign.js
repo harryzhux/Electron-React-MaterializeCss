a138233d4128f7dbbd7c9239d17692d1cd5403b2
harryzhux <harryzhux@gmail.com>
2017-09-09 21:14:07 -0700

    add slider

--- js/MaterialDesign.js
+++ js/MaterialDesign.js
@@ -6,20 +6,17 @@ import FixedActionButton from './FixedActionButton.js';
 import NavbarTabs from './NavbarTabs.js';
 import FloatActionButton from './FloatActionButton.js';
 import Modal from './Modal.js';
+import Slider from './Slider';
 
 class MaterialDesign extends React.Component {
   render() {
     return (
       <div className="container">
           <DropDownMenu />
-          <img src="../images/NetworkComputer.png" />
-          <FloatActionButton />
-
 	  <NavbarTabs />
-
+          <Slider />
+          <FloatActionButton />
 	  <Modal />
-
-
       </div>
     );
   }
--- js/Slider.js
+++ js/Slider.js
@@ -15,21 +15,45 @@ class Slider extends React.Component {
     <ul className="slides">
       <li className="active">
         <img src="../images/FlowTracer.png" />
+        <div className="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 className="light grey-text text-lighten-3">Here is our small slogan.</h5>
+        </div>
       </li>
       <li>
         <img src="../images/LicenseAllocator.png" />
+        <div className="caption left-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 className="light grey-text text-lighten-3">Here is our small slogan.</h5>
+        </div>
       </li>
       <li>
         <img src="../images/LicenseMonitor.png" />
+        <div className="caption right-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 className="light grey-text text-lighten-3">Here is our small slogan.</h5>
+        </div>
       </li>
       <li>
         <img src="../images/NetworkComputer.png" />
+        <div className="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 className="light grey-text text-lighten-3">Here is our small slogan.</h5>
+        </div>
       </li>
       <li>
         <img src="../images/WorkloadXelerator.png" />
+        <div className="caption right-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 className="light grey-text text-lighten-3">Here is our small slogan.</h5>
+        </div>
       </li>
       <li>
         <img src="../images/HERO.png" />
+        <div className="caption left-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 className="light grey-text text-lighten-3">Here is our small slogan.</h5>
+        </div>
       </li>
     </ul>
     <ul className="indicators">
